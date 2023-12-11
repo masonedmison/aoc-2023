25fce96661cff886ed6ffb694071168a7a94fa03
Mason Edmison <mason.edmison@gmail.com>
2023-12-11 15:59:30 -0600

    Day 06 part 1

--- src/Day06.hs
+++ src/Day06.hs
@@ -3,10 +3,56 @@ module Day06 where
 import Paths_aoc2023 (getDataFileName)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
+import Data.Attoparsec.Text
+import Utils (digitsToInt)
+
+data Race = Race {
+  time :: Int,
+  distance :: Int
+}
+
+dataParser :: Parser [Race]
+dataParser =
+  do
+    string "Time:"
+    skipSpace
+    times <- sepBy (many1 digit) (many1 (char ' '))
+    let timeInts = digitsToInt <$> times
+    many' endOfLine
+    string "Distance:"
+    skipSpace
+    dists <- sepBy (many1 digit) (many1 (char ' '))
+    let distInts = digitsToInt <$> dists
+    let races = fmap (\(t, d) -> Race {time = t, distance = d}) (timeInts `zip` distInts)
+    return races
+
+-- length held, distance
+calculateDistance :: Int -> Int -> Int
+calculateDistance lh t = (t - lh) * lh
+
+calculateNumWins :: Race -> Int
+calculateNumWins r =
+  length (increaseUntilLose 0 [])
+  where
+    d = distance r
+    t = time r
+    -- length held, acc
+    increaseUntilLose :: Int -> [Int] -> [Int]
+    increaseUntilLose lh acc
+      | lh >= t = acc
+      | otherwise =
+        let travelled = calculateDistance lh t
+        in
+          if travelled > d then increaseUntilLose (lh + 1) (lh:acc)
+          else increaseUntilLose (lh + 1) acc
+
 
 day06 :: IO ()
 day06 = do
-  inputLines <- T.lines <$> (getDataFileName "day06-input.txt" >>= TIO.readFile)
+  inputLines <- getDataFileName "day06-input.txt" >>= TIO.readFile
   putStrLn "This is what I read from input:"
-  TIO.putStrLn $ T.unlines inputLines
-  putStrLn "TODO: implement Day 06"
+  TIO.putStrLn inputLines
+  let racesEither = parseOnly dataParser inputLines
+  races <- either fail pure racesEither
+  let productOfWins = foldr ((*) . calculateNumWins) 1 races
+  print productOfWins
