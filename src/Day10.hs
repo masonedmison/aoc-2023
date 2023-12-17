6b2ba5c44515a0c4fe93367c8d082fdbf34e2f25
Mason Edmison <mason.edmison@gmail.com>
2023-12-17 13:32:30 -0600

    day 10 part2

--- src/Day10.hs
+++ src/Day10.hs
@@ -1,13 +1,17 @@
 {-# LANGUAGE TupleSections #-}
+
 module Day10 where
 
-import Paths_aoc2023 (getDataFileName)
-import qualified Data.Vector as V
+import qualified Data.Map.Strict as M
 import Data.Maybe (mapMaybe)
 import qualified Data.Set as S
-import Debug.Trace (trace)
+import Data.Tuple.All (Sel3 (..))
+import qualified Data.Vector as V
+import Paths_aoc2023 (getDataFileName)
 
 type Coord = (Int, Int)
+
+type LoopCoords = M.Map Coord Char
 
 -- length of columns X length of rows
 type UpperBounds = (Int, Int)
@@ -15,12 +19,16 @@ type UpperBounds = (Int, Int)
 type Visited = S.Set Coord
 
 type Maze = V.Vector (V.Vector Char)
+
 fromList :: [[Char]] -> Maze
 fromList chs =
   V.fromList $ V.fromList <$> chs
 
 get :: Maze -> Coord -> Char
 get mz (x, y) = mz V.! y V.! x
+
+replace :: Maze -> Coord -> Char -> Maze
+replace mz (x, y) ch = V.update mz (V.singleton (y, V.update (mz V.! y) (V.singleton (x, ch))))
 
 getBounds :: Maze -> UpperBounds
 getBounds mz =
@@ -72,11 +80,11 @@ validTransition' dir chL chR = case (chL, chR) of
     W -> validWestDirs chR
     _ -> False
   _ -> False
-  
 
 validTransition :: Direction -> Char -> Char -> Maybe Char
 validTransition dir srcCh destCh =
-    if validTransition' dir srcCh destCh then Just destCh
+  if validTransition' dir srcCh destCh
+    then Just destCh
     else Nothing
 
 findNeighbors :: UpperBounds -> Coord -> [Coord]
@@ -95,10 +103,7 @@ getValidNeighbors :: UpperBounds -> Maze -> Char -> Coord -> [(Coord, Char)]
 getValidNeighbors ubounds mz srcCh coord =
   let neighbors = findNeighbors ubounds coord
       withDirs = zip neighbors (dirFromCoord coord <$> neighbors)
-    in
-      -- trace ("mapMaybe with withDirs = " ++ show withDirs)
-      mapMaybe (\(dest, dir) -> fmap (dest,) (validTransition dir srcCh (get mz dest))) withDirs
-      -- head (filter (\(c, _) -> S.notMember c visited) valids)
+   in mapMaybe (\(dest, dir) -> fmap (dest,) (validTransition dir srcCh (get mz dest))) withDirs
 
 findStart :: Maze -> Coord
 findStart mz =
@@ -106,37 +111,53 @@ findStart mz =
     Just v -> v
     Nothing -> error "expected 'S' starting point in maze."
   where
-    findS = V.findIndex (=='S')
+    findS = V.findIndex (== 'S')
     maybeS mz =
       do
-      y <- V.findIndex (not . null . findS) mz
-      x <- findS (mz V.! y)
-      return (x, y)
+        y <- V.findIndex (not . null . findS) mz
+        x <- findS (mz V.! y)
+        return (x, y)
 
-
-findLengthOfLoop :: Maze -> Int
-findLengthOfLoop mz =
-  trace ("starting go with stChar: " ++ show stChar ++ " and stCoord: " ++ show stCoord) 
-  go stChar stCoord (S.fromList[s, stCoord]) 1
+getLoopCoords :: UpperBounds -> Maze -> LoopCoords
+getLoopCoords bounds mz =
+  go stChar stCoord (M.fromList [(s, 'S'), (stCoord, stChar)])
   where
-    bounds = getBounds mz
-    -- st = starting coordinate of S
+    -- s = starting coordinate of S
     s = findStart mz
     -- by definition, s should have exactly two neighbors
     -- choose one to start go with (with count = 1) and record the other
     -- as the termination check
     -- we should add both stCoord, and s to visited upon starting the recursion of go
-    ((stCoord, stChar):(endCoord, _):_) = getValidNeighbors bounds mz 'S' s
-    go currChar currCoord visited count
-      | currCoord == endCoord && count /= 0 = count + 1
+    ((stCoord, stChar) : (endCoord, _) : _) = getValidNeighbors bounds mz 'S' s
+    go currChar currCoord visited
+      | currCoord == endCoord = visited
       | otherwise =
-        -- trace ("Calling getValidNeighors with currChar: " ++ show currChar ++ " currCoord: " ++ show currCoord ++ " visited: " ++ show visited) 
-        let neighbors = getValidNeighbors bounds mz currChar currCoord
-            -- trace ("calling head with neighbors: " ++ show neighbors ++ " visited: " ++ show visited)
-            (nextCoord, nextChar) =  (head (filter (\(c, _) -> S.notMember c visited) neighbors))
-          in
-            --trace ("recursing in go with nextChar: " ++ show nextChar ++ " nextCoord: " ++ show nextCoord) 
-            go nextChar nextCoord (nextCoord `S.insert` visited) count + 1
+          let neighbors = getValidNeighbors bounds mz currChar currCoord
+              (nextCoord, nextChar) = head (filter (\(c, _) -> M.notMember c visited) neighbors)
+           in go nextChar nextCoord (M.insert nextCoord nextChar visited)
+
+scanLine :: V.Vector (Char, Coord) -> LoopCoords -> Int
+scanLine line coords =
+  sel3 (V.foldl (\(io, pc, c) entry -> adjustState entry io pc c) (False, ' ', 0) line)
+  where
+    isCorner char = char `elem` ['L', 'F', 'J', '7']
+    inLoopCoords coord = M.member coord coords
+    adjustState (ch, coord) inout prevCorner count = case ch of
+      _ | isCorner ch && inLoopCoords coord -> case (prevCorner, ch) of
+        ('F', 'J') -> (not inout, ' ', count)
+        ('L', '7') -> (not inout, ' ', count)
+        (_, 'L') -> (inout, 'L', count)
+        (_, 'F') -> (inout, 'F', count)
+        _ -> (inout, prevCorner, count)
+      '|' | inLoopCoords coord -> (not inout, ' ', count)
+      _ | inout && not (inLoopCoords coord) -> (inout, prevCorner, count + 1)
+      _ -> (inout, prevCorner, count)
+
+scanLines :: Maze -> LoopCoords -> Int
+scanLines mz coords =
+  V.foldr (\l accCount -> accCount + scanLine l coords) 0 indexedMaze
+  where
+    indexedMaze = V.map (\(y, xs) -> V.map (\(x, ch) -> (ch, (x, y))) (V.indexed xs)) (V.indexed mz)
 
 parseIntoMaze :: IO Maze
 parseIntoMaze =
@@ -146,12 +167,20 @@ parseIntoMaze =
     putStrLn $ unlines inputLines
     return (fromList inputLines)
 
+adjustS :: Maze -> Maze
+adjustS mz =
+  replace mz sCoord 'J'
+  where
+    sCoord = findStart mz
+
 day10 :: IO ()
 day10 = do
   inputLines <- lines <$> (getDataFileName "day10-input.txt" >>= readFile)
   putStrLn "This is what I read from input:"
   putStrLn $ unlines inputLines
-  let mz = fromList inputLines
-  let loopLen = findLengthOfLoop mz
-  let farthest = ceiling (fromIntegral loopLen / 2)
-  print farthest
+  let mz' = fromList inputLines
+  let bounds = getBounds mz'
+  let loopCoords = getLoopCoords bounds mz'
+  let mz = adjustS mz'
+  let enc = scanLines mz loopCoords
+  print enc
