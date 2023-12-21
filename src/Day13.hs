26a4cf26fdf01fee648b2f0ce76304f5abeca722
Mason Edmison <mason.edmison@gmail.com>
2023-12-21 16:33:34 -0600

    remove unncessary cruft

--- src/Day13.hs
+++ src/Day13.hs
@@ -8,31 +8,15 @@ import Data.List.Split
 
 type Grid = Vector (Vector Char)
 
-data SplitPoint = Clean | Dup deriving (Show, Eq)
-
 parseInput :: String -> [Grid]
 parseInput st =
   fmap (V.fromList . fmap V.fromList) spl
   where
     spl = (map lines . splitOn "\n\n") st
 
-adjustCol :: (SplitPoint, Int) -> Int
-adjustCol (Clean, n) = n
-adjustCol (Dup, n) = n
-
-checkReflection :: SplitPoint -> Int -> V.Vector Char -> Bool
-checkReflection Dup n chs
-  | n  > 0 && (n + 1) < V.length chs =
-    foldr step True indicesToCheck
-    where
-      step (x, y) b
-        | chs ! x == chs ! y = b
-        | otherwise = False
-      indicesToCheck = zip [n,n-1..0] [n..V.length chs - 1]
-
-checkReflection Clean n chs 
+checkReflection :: Int -> V.Vector Char -> Bool
+checkReflection n chs 
   | (n - 1) > 0 && (n + 1) < V.length chs =
-    -- trace ("indices to check: " ++ show indicesToCheck) 
     foldr step True indicesToCheck
     where
       step (x, y) b
@@ -40,30 +24,26 @@ checkReflection Clean n chs
         | otherwise = False
       indicesToCheck = zip [n-1,n-2..0] [n..V.length chs - 1]
 
-checkReflection _ _ _ = False
+checkReflection _ _ = False
 
 -- find initial
-findAllReflections :: Vector Char -> [(SplitPoint, Int)]
+findAllReflections :: Vector Char -> [Int]
 findAllReflections chs | V.length chs < 2 = []
 findAllReflections chs =
   foldr step [] args
   where
-    step (spl, n) acc
-      | checkReflection spl n chs = col : acc
-        where
-          col = case spl of
-            Dup -> (Dup, n)
-            Clean -> (Clean, n)
+    step n acc
+      | checkReflection n chs = n : acc
     step _ acc = acc
-    args = [1..V.length chs - 1] >>= \i -> [(Clean, i), (Dup, i)]
+    args = [1..V.length chs - 1]
 
 compute :: Grid -> [Int]
 compute rows | not (null rows) =
-  adjustCol <$> foldr step [] firstReflections
+  foldr step [] firstReflections
   where
     t = V.tail rows
-    step (spl, n) acc
-      | all (checkReflection spl n) t = (spl, n) : acc
+    step n acc
+      | all (checkReflection n) t = n : acc
     step _  acc = acc
     firstReflections = findAllReflections (V.head rows)
 compute _ = pure 0
