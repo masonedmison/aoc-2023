8f1f2a322e38437fb47e501bf07ec81a868c8724
Mason Edmison <mason.edmison@gmail.com>
2023-12-22 12:22:16 -0600

    day 14 part 1

--- src/Day14.hs
+++ src/Day14.hs
@@ -1,12 +1,35 @@
 module Day14 where
 
 import Paths_aoc2023 (getDataFileName)
-import qualified Data.Text as T
-import qualified Data.Text.IO as TIO
+import Data.List (transpose)
+
+type Grid = [[Char]]
+
+-- running total of load, number of periods
+type State = (Int, Int)
+
+processRow :: State -> [(Int, Char)] -> Int
+processRow (currTotal, _) [] = currTotal
+processRow (currTotal, _) ((_, '#'):rest) = processRow (currTotal, 0) rest
+processRow (currTotal, currPeriods) ((_, '.'):rest) = processRow (currTotal, currPeriods + 1) rest
+processRow (currTotal, currPeriods) ((idx, 'O'):rest) = processRow (updatedTotal, currPeriods) rest
+  where
+    updatedTotal = idx + currPeriods + currTotal
+
+processRow _ _ = error "unexpected char encountered."
+
+compute :: Grid -> Int
+compute gr | not (null gr) =
+  sum (fmap (processRow initialState . zip [maxCol, maxCol - 1 .. 1]) transposedGrid)
+  where
+    maxCol = length (head gr)
+    transposedGrid = transpose gr
+    initialState = (0, 0)
+compute _ = 0
 
 day14 :: IO ()
 day14 = do
-  inputLines <- T.lines <$> (getDataFileName "day14-input.txt" >>= TIO.readFile)
+  inputLines <- lines <$> (getDataFileName "day14-input.txt" >>= readFile)
   putStrLn "This is what I read from input:"
-  TIO.putStrLn $ T.unlines inputLines
-  putStrLn "TODO: implement Day 14"
+  let result = compute inputLines
+  print result
