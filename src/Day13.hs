ddb7dccbefc98d75e31f4ec4f547093b8b8649fb
Mason Edmison <mason.edmison@gmail.com>
2023-12-22 11:37:30 -0600

    day 13 part 2

--- src/Day13.hs
+++ src/Day13.hs
@@ -15,47 +15,59 @@ parseInput st =
   where
     spl = (map lines . splitOn "\n\n") st
 
-checkReflection :: Int -> V.Vector Char -> Bool
-checkReflection n chs 
+checkReflection :: Int -> Int -> V.Vector Char -> Bool
+checkReflection missCount n chs
   | n > 0 && n < V.length chs =
-    foldr step True indicesToCheck
+    (actualMisses == missCount) && result
     where
-      step (x, y) b
-        | chs ! x == chs ! y = b
-        | otherwise = False
+      (actualMisses, result) = foldr step (0, True) indicesToCheck
+      step (x, y) (missAcc, b)
+        | chs ! x == chs ! y = (missAcc, b)
+        | missAcc < missCount = (missAcc + 1, b)
+        | otherwise = (-1, False)
       indicesToCheck = zip [n-1,n-2..0] [n..V.length chs - 1]
 
-checkReflection _ _ = False
+checkReflection _ _ _ = False
 
 -- find initial
-findAllReflections :: Vector Char -> [Int]
-findAllReflections chs | V.length chs < 2 = []
-findAllReflections chs =
+findAllReflections :: Int -> Vector Char -> [Int]
+findAllReflections _ chs | V.length chs < 2 = []
+findAllReflections missCount chs =
   foldr step [] args
   where
     step n acc
-      | checkReflection n chs = n : acc
+      | checkReflection missCount n chs = n : acc
     step _ acc = acc
     args = [1..V.length chs - 1]
 
-compute :: Grid -> [Int]
-compute rows | not (null rows) =
+-- first row miss, accum miss
+compute :: Int -> Int -> Grid -> [Int]
+compute firstRowMiss accumMisses rows | not (null rows) =
   foldr step [] firstReflections
   where
     t = V.tail rows
     step n acc
-      | all (checkReflection n) t = n : acc
+      | length t - length (V.filter (checkReflection 0 n) t) == accumMisses = n : acc
     step _  acc = acc
-    firstReflections = findAllReflections (V.head rows)
-compute _ = pure 0
+    firstReflections = findAllReflections firstRowMiss (rows ! 0)
+compute _ _ _ = pure 0
 
-part1 :: [Grid] -> Int
+part1, part2 :: [Grid] -> Int
 part1 grs =
   sum $ single <$> grs
   where
     single gr = sum (horizontal gr) + sum ((*100) <$> vertical gr)
-    horizontal = compute
-    vertical = compute . transposeVec
+    horizontal = compute 0 0
+    vertical = compute 0 0 . transposeVec
+
+part2 grs =
+  sum $ single <$> grs
+  where
+    single gr = sum (horizontalF gr ++ horizontalS gr) + sum ((*100) <$> (verticalF gr ++ verticalS gr))
+    horizontalF = compute 1 0
+    horizontalS = compute 0 1
+    verticalF = compute 1 0 . transposeVec
+    verticalS = compute 0 1 . transposeVec
 
 day13 :: IO ()
 day13 = do
@@ -63,5 +75,5 @@ day13 = do
   putStrLn "This is what I read from input:"
   putStrLn input
   let grids = parseInput input
-  let result = part1 grids
+  let result = part2 grids
   timeIt $ print result
