532699f16951efef354040a0ccf400138a208cb9
Mason Edmison <mason.edmison@gmail.com>
2023-12-26 17:46:36 -0600

    day 15 part 1

--- src/Day15.hs
+++ src/Day15.hs
@@ -1,12 +1,25 @@
 module Day15 where
 
 import Paths_aoc2023 (getDataFileName)
-import qualified Data.Text as T
-import qualified Data.Text.IO as TIO
+import Data.List.Extra (splitOn)
+import Data.Foldable (foldl')
+import Data.Char (ord)
+
+type Input = [[Char]]
+
+parseInput :: String -> Input
+parseInput = splitOn ","
+
+part1 :: Input -> Int
+part1 inp =
+  sum $ processSeq <$> inp
+  where
+    processSeq = foldl' step 0
+    step acc ch = ((acc + ord ch) * 17)  `mod` 256
 
 day15 :: IO ()
 day15 = do
-  inputLines <- T.lines <$> (getDataFileName "day15-input.txt" >>= TIO.readFile)
+  input <- getDataFileName "day15-input.txt" >>= readFile
   putStrLn "This is what I read from input:"
-  TIO.putStrLn $ T.unlines inputLines
-  putStrLn "TODO: implement Day 15"
+  putStrLn input
+  print $ part1 $ parseInput input
