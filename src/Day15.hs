c8391a76cedf69b6632e97d543b51a83f5f601bc
Mason Edmison <mason.edmison@gmail.com>
2023-12-27 10:33:01 -0600

    day 15 part 2

--- src/Day15.hs
+++ src/Day15.hs
@@ -1,25 +1,55 @@
 module Day15 where
 
-import Paths_aoc2023 (getDataFileName)
-import Data.List.Extra (splitOn)
+import Data.Char
 import Data.Foldable (foldl')
-import Data.Char (ord)
+import Data.List.Split
+import Data.Map.Strict (Map, adjust, empty, insert, toList, (!))
+import Paths_aoc2023 (getDataFileName)
 
-type Input = [[Char]]
+type BoxEntry = (String, Int)
+
+type Boxes = Map Int [BoxEntry]
+
+new :: Boxes
+new = foldl' (\acc i -> insert i [] acc) empty [0 .. 255]
+
+type Input = [String]
 
 parseInput :: String -> Input
-parseInput = splitOn ","
+parseInput = splitOn "," . head . lines
 
-part1 :: Input -> Int
-part1 inp =
-  sum $ processSeq <$> inp
+hash :: String -> Int
+hash =
+  foldl' step 0
   where
-    processSeq = foldl' step 0
-    step acc ch = ((acc + ord ch) * 17)  `mod` 256
+    step acc ch = ((acc + ord ch) * 17) `mod` 256
+
+processEntry :: Boxes -> String -> Boxes
+processEntry bs =
+  go . span (`notElem` ['-', '='])
+  where
+    go (label, "-") = adjust (filter ((/= label) . fst)) (hash label) bs
+    go (label, _ : n)
+      | label `elem` labs = adjust (const $ before ++ [(label, read n)] ++ after) hashed bs
+      | otherwise = adjust (++ [(label, read n)]) hashed bs
+      where
+        hashed = hash label
+        entries = bs ! hashed
+        labs = fmap fst entries
+        (before, _ : after) = span ((/= label) . fst) entries
+    go _ = error "ruh roh"
+
+calculateBoxTotal :: Boxes -> Int
+calculateBoxTotal bs =
+  sum $ toList bs >>= uncurry calculateBox
+  where
+    calculateBox boxN bx =
+      (\(i, (_, len)) -> (boxN + 1) * i * len) <$> zip [1 ..] bx
 
 day15 :: IO ()
 day15 = do
-  input <- getDataFileName "day15-input.txt" >>= readFile
+  input <- parseInput <$> (getDataFileName "day15-input.txt" >>= readFile)
   putStrLn "This is what I read from input:"
-  putStrLn input
-  print $ part1 $ parseInput input
+  let bs = foldl' processEntry new input
+  let result = calculateBoxTotal bs
+  print result
