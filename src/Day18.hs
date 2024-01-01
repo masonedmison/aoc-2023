324ec264104d940e229b541282a45c54263a978c
Mason Edmison <mason.edmison@gmail.com>
2023-12-31 18:24:04 -0600

    day 18 part 1

--- src/Day18.hs
+++ src/Day18.hs
@@ -1,12 +1,45 @@
 module Day18 where
 
 import Paths_aoc2023 (getDataFileName)
-import qualified Data.Text as T
-import qualified Data.Text.IO as TIO
+import Data.List.Extra (splitOn, scanl')
+
+type Input = [(Char, Int, String)]
+type Coord = (Int, Int)
+type Lagoon = [Coord]
+digTrench :: Input -> Lagoon
+digTrench = scanl' digCorner (0, 0)
+  where
+    digCorner (x, y) ('R', n, _) = (x + n, y)
+    digCorner (x, y) ('L', n, _) = (x - n, y)
+    digCorner (x, y) ('U', n, _) = (x, y - n)
+    digCorner (x, y) ('D', n, _) = (x, y + n)
+    digCorner _ _ = error "ruh roh!"
+
+area :: Lagoon -> Int
+area lag =
+   1 + perimeter `div` 2 + (abs . (`div` 2) . sum . zipWith crossProduct lag $ tail lag ++ [head lag])
+  where
+    crossProduct (x0, y0) (x1, y1) = x0 * y1 - x1 * y0  
+    dist (x0, y0) (x1, y1) = abs (x0 - x1) + abs (y0 - y1) 
+    perimeter = sum . zipWith dist lag $ (tail lag ++ [head lag])
+
+part1 :: Input -> Int
+part1 inp =
+  area trenchCorners
+  where
+    trenchCorners = digTrench inp
+
+parseInput :: String -> Input
+parseInput =
+  fmap (fmap go) segs
+  where
+    segs st = splitOn [' '] <$> lines st
+    go ((fc:_):n:c:_) = (fc, read n :: Int, c)
+    go _ = error "expected 3 elements."
 
 day18 :: IO ()
 day18 = do
-  inputLines <- T.lines <$> (getDataFileName "day18-input.txt" >>= TIO.readFile)
-  putStrLn "This is what I read from input:"
-  TIO.putStrLn $ T.unlines inputLines
-  putStrLn "TODO: implement Day 18"
+  inputS <- getDataFileName "day18-input.txt" >>= readFile
+  let inp = parseInput inputS
+  let result = part1 inp
+  print result
